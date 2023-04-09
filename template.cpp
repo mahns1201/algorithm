a038a260f68a379f4619fa34156182c55eb6bb76
서민혁 <mahns1201@gmail.com>
2023-04-09 23:46:07 +0900

    tab size 4

--- template.cpp
+++ template.cpp
@@ -2,9 +2,8 @@
 using namespace std;
 
 int main() {
-  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-  
-  cout << "hello, world!" << "\n";
-  return 0;
-}
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
+    cout << "hello, world!" << "\n";
+    return 0;
+}
