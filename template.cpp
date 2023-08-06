c4f1d527df80a2f0662e4ce0261158585d273b89
서민혁 <mahns1201@gmail.com>
2023-08-07 00:47:47 +0900

    ADD: print vector value function

--- template.cpp
+++ template.cpp
@@ -1,6 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void printV(vector<int> V) {
+    for (auto i: V) {
+        cout << i << "\n";
+    }
+}
+
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
