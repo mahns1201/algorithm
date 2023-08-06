69c985d4a277536aca3529d3207a27867756d442
서민혁 <mahns1201@gmail.com>
2023-08-07 01:07:40 +0900

    [8-2559-s3] +2 *

--- no/1weeks/8-2559-s3.cpp
+++ no/1weeks/8-2559-s3.cpp
@@ -29,10 +29,40 @@ int main() {
     result[j] = pSum[j+space-1] - pSum[j-1];
   }
 
-  sort(result, result+R, greater<int>());
+  sort(result, result+R, greater<int>()); // max 함수로 개선하자
 
   cout << result[0] << "\n";
 
   return 0;
 }
 
+/*
+	날짜: 2023-08-07
+	횟수: 2
+*/
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int N, K;
+// int preSum[100004];
+// vector<int> ret;
+
+// int main() {
+//     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+//     cin >> N >> K;
+    
+//     int n;
+//     for (int i=0; i<N; i++) {
+//         cin >> n;
+//         preSum[i+1] = preSum[i] + n;
+//     }
+
+//     for (int j=K; j<N+1; j++) {
+//         ret.push_back(preSum[j] - preSum[j-K]);
+//     }
+
+//     sort(ret.begin(), ret.end(), greater<int>());
+//     cout << ret[0] << "\n";
+
+//     return 0;
+// }
