a97746f9534d11c3736b38051ee83c994414b041
서민혁 <mahns1201@gmail.com>
2023-08-07 22:43:23 +0900

    [12-1940-s4] +2

--- baekjoon/1weeks/12-1940-s4.cpp
+++ baekjoon/1weeks/12-1940-s4.cpp
@@ -32,3 +32,33 @@ int main() {
 
   return 0;
 }
+
+
+/*
+	날짜: 2023-08-07
+	횟수: 2
+  특이사항: 3회차에선 cnt/2를 없앨 수 있는 방법을 생각해봐라.
+*/
+// int N, M, cnt;
+// int main() {
+//     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+//     cin >> N >> M;
+//     int arr[N];
+//     for (int i=0; i<N; i++) {
+//         int temp;
+//         cin >> temp;
+//         arr[i] = temp;
+//     }
+
+//     for (int j: arr) {
+//         for (int k: arr) {
+//             if (M == j+k) {
+//                 cnt++;
+//             }
+//         }
+//     }
+
+//     cout << cnt/2 << "\n";
+
+//     return 0;
+// }
