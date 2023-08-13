56083fdab5ffdce867d9f1f9659d722afcd9c6aa
서민혁 <mahns1201@gmail.com>
2023-08-13 18:16:58 +0900

    [1-2178-s1] +2 *

--- baekjoon/2weeks/1-2178-s1.cpp
+++ baekjoon/2weeks/1-2178-s1.cpp
@@ -1,3 +1,5 @@
+// https://www.acmicpc.net/problem/2178
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -73,3 +75,51 @@ int main() {
 1 1 1 1 1 1
 1 1 1 1 0 1
 */
+
+
+/*
+	날짜: 2023-08-13
+	횟수: 2
+  특이사항: 1회독에서 한 것 처럼 bfs() 함수화 해서 풀어보기
+*/
+// int maxV = 104;
+// int N, M, y, x;
+// int dy[] = {-1, 0, 1, 0};
+// int dx[] = {0, 1, 0, -1};
+// int m[104][104];
+// int visited[104][104];
+
+// int main() {
+//     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+//     cin >> N >> M;
+
+//     for (int i=0; i<N; i++) {
+//         string t;
+//         cin >> t;
+//         for (int j=0; j<t.size(); j++) {
+//             m[i][j] = t[j] - 48;
+//         }
+//     }
+
+//     visited[0][0] = 1;
+//     queue<pair<int, int>> q;
+//     q.push({0, 0});
+//     while(q.size()) {
+//         tie(y, x) = q.front(); q.pop();
+
+//         for (int i=0; i<4; i++) {
+//             int ny = y + dy[i];
+//             int nx = x + dx[i];
+//             if (ny < 0 || nx < 0 || ny > maxV || nx >> maxV) continue;
+//             if (!m[ny][nx]) continue;
+//             if (visited[ny][nx]) continue;
+
+//             visited[ny][nx] = visited[y][x] + 1;
+//             q.push({ny, nx});
+//         }
+//     }
+    
+//     cout << visited[N-1][M-1] << "\n";
+
+//     return 0;
+// }
