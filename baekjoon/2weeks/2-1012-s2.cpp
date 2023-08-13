fe24c73e7b7a3064311df4ed2ec8db02f1d16525
서민혁 <mahns1201@gmail.com>
2023-08-14 00:37:11 +0900

    [2-1012-s2] +2 *

--- baekjoon/2weeks/2-1012-s2.cpp
+++ baekjoon/2weeks/2-1012-s2.cpp
@@ -68,3 +68,60 @@ int main() {
   
   return 0;
 }
+
+/*
+	날짜: 2023-08-14
+	횟수: 2
+*/
+// int T, M, N, K;
+// int dy[] = {-1, 0, 1, 0};
+// int dx[] = {0, 1, 0, -1};
+// int m[54][54];
+// int visited[54][54];
+
+// void dfs(int y, int x) {
+//     visited[y][x] = 1;
+
+//     for (int t=0; t<4; t++) {
+//         int ny = y + dy[t];
+//         int nx = x + dx[t];
+
+//         if (ny < 0 || 54 < ny || nx < 0 || 54 < nx) continue;
+//         if (!m[ny][nx]) continue;
+//         if (visited[ny][nx]) continue;
+
+//         dfs(ny, nx);
+//     }
+// }
+
+// int main() {
+//     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+//     cin >> T;
+//     while (0<T) {
+//         T--;
+//         cin >> M >> N >> K;
+
+//         fill(&m[0][0], &m[53][54], 0);
+//         fill(&visited[0][0], &visited[53][54], 0);
+
+//         for (int t=0; t<K; t++) {
+//             int x, y;
+//             cin >> x >> y; // M: y, N: x
+//             m[y][x] = 1;
+//         }
+
+//         int ret = 0;
+//         for (int i=0; i<N; i++) {
+//             for (int j=0; j<M; j++) {
+//                 if (!visited[i][j] && m[i][j]) {
+//                     dfs(i, j);
+//                     ret++;
+//                 }
+//             }
+//         }
+
+//         cout << ret << "\n";
+//     }
+
+//     return 0;
+// }
