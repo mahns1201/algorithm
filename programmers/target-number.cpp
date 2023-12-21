86c1e44313fe7225fac1d87e4dd544cf689c2369
서민혁 <mahns1201@gmail.com>
2023-12-21 16:28:47 +0900

    [타겟넘버-lv2] +2

--- programmers/target-number.cpp
+++ programmers/target-number.cpp
@@ -25,3 +25,35 @@ int solution(vector<int> numbers, int target) {
   rFn(numbers, target, 0);
   return answer;
 }
+
+/*
+	날짜: 2023-12-21
+	횟수: 2
+*/
+// #include <string>
+// #include <vector>
+
+// using namespace std;
+
+// int answer = 0;
+// int t_target;
+// vector<int> t_numbers;
+
+// void dfs(int curr, int depth) {
+//     if (depth == t_numbers.size()) {
+//         if (curr == t_target) answer++;
+//         return;
+//     }
+    
+//     dfs (curr + t_numbers[depth], depth + 1);
+//     dfs (curr - t_numbers[depth], depth + 1);
+// }
+
+// int solution(vector<int> numbers, int target) {
+//     t_numbers = numbers;
+//     t_target = target;
+    
+//     dfs(0, 0);
+    
+//     return answer;
+// }
