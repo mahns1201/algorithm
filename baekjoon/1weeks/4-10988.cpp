4f77ecc64ce27927de3c66ae070bbc2ba599a201
서민혁 <mahns1201@gmail.com>
2023-12-21 19:04:57 +0900

    [10988-b2] +3

--- baekjoon/1weeks/4-10988.cpp
+++ baekjoon/1weeks/4-10988.cpp
@@ -3,6 +3,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+/*
+	날짜: 2023-12-21
+	횟수: 3
+*/
 string input;
 int ret = 0;
 int main() {
@@ -20,3 +25,34 @@ int main() {
   
   cout << ret << "\n";
 }
+
+// string s;
+// int main() {
+//     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+//     cin >> s;
+
+//     if (s.size() == 1) {
+//         cout << "1\n";
+//         exit(0);
+//     }
+
+//     int si = 0;
+//     int se = s.size() - 1;
+    
+//     while(true) {
+//         if (si >= se) {
+//             cout << "1\n";
+//             exit(0);
+//         }
+
+//         if (s[si] != s[se]) {
+//             cout << "0\n";
+//             exit(0);
+//         }
+
+//         si++;
+//         se--;
+//     }
+
+//     return 0;
+// }
