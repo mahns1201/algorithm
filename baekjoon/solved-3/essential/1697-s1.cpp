07eae035aed38e72ff6518c15d7d7fd0ca259a6f
서민혁 <mahns1201@gmail.com>
2024-02-12 05:18:23 +0900

    [1697-s1] +2*

--- baekjoon/solved-3/essential/1697-s1.cpp
+++ baekjoon/solved-3/essential/1697-s1.cpp
@@ -3,8 +3,9 @@ using namespace std;
 
 /*
 	날짜: 2024-02-11
-	횟수: 1
+	횟수: 2
     메모: 그래프 탐색 기본에 충실하자: visited, 방문 유효성(!visited[x], 범위 확인)
+    메모(2): visited = 1 -> 호출과 호출 -> visited = 1의 차이
 */
 
 int n, m, x, depth;
@@ -53,3 +54,45 @@ int main() {
 
     return 0;
 }
+
+// const int max_val = 100004;
+// int n, k, x, y;
+// int visited[max_val];
+// bool is_valid(int x) {
+//     if (x<0 || x>100000 || visited[x]) return false;
+//     else return true;
+// }
+// int main() {
+//     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+//     cin >> n >> k;
+
+//     queue<pair<int,int>> q;
+//     q.push({n, 0});
+//     visited[n] = 1;
+//     while (!q.empty()) {
+//         tie(x, y) = q.front(); q.pop();
+//         if (x==k) {
+//             cout << y << "\n";
+//             break;
+//         }
+//         if (x+1==k || x*2==k || x-1==k) {
+//             cout << y+1 << "\n";
+//             break;
+//         }
+        
+//         if (is_valid(x+1)) {
+//             visited[x+1] = 1;
+//             q.push({x+1, y+1});
+//         }
+//         if (is_valid(x*2)) {
+//             visited[x*2] = 1;
+//             q.push({x*2, y+1});
+//         }
+//         if (is_valid(x-1)) {
+//             visited[x-1] = 1;
+//             q.push({x-1, y+1});
+//         }
+//     }
+
+//     return 0;
+// }
