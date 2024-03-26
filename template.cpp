38f2d1ae48c3b7e9dd6a8848e87f584c9b007b68
서민혁 <mahns1201@gmail.com>
2024-03-27 01:04:49 +0900

    DEL/FIX: printV/using '\n'

--- template.cpp
+++ template.cpp
@@ -1,15 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void printV(vector<int> V) {
-    for (auto i: V) {
-        cout << i << "\n";
-    }
-}
-
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-    cout << "hello, world!" << "\n";
+    cout << "hello, world!" << '\n';
     return 0;
 }
