0a29d6ba262387f3e388c3531afc39debdfc2a45
서민혁 <mahns1201@gmail.com>
2024-04-02 03:30:31 +0900

    ADD: annotation example

--- template.cpp
+++ template.cpp
@@ -1,6 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+	날짜: yyyy-mm-dd
+	횟수: unsigned int
+    메모: add commit message with '*'
+*/
+
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
