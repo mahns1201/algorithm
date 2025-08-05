1ed7fed63762166ac9041706015fcc0a4987ff54
서민혁 <mahns1201@gmail.com>
2025-08-05 21:08:01 +0900

    substr, split

--- note/string.cpp
+++ note/string.cpp
@@ -1,2 +1,21 @@
-string s = "it will be reversed";
-reverse(s.begin(), s.end());
+// 문자열 뒤집기
+string s1 = "it will be reversed";
+reverse(s1.begin(), s1.end());
+
+// 문자열 자르기
+string s2 = "it will be sliced";
+s2.substr(0, s2.size());
+
+// split
+vector<string> split(string input, string delimiter) { 
+    vector<string> ret;
+    long long pos = 0;
+    string token = "";
+    while ((pos = input.find(delimiter)) != string::npos) { 
+        token = input.substr(0, pos); 
+        ret.push_back(token);
+        input.erase(0, pos + delimiter.length());
+    } 
+    ret.push_back(input); 
+    return ret;
+}
