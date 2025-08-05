7f8f53c3852ea0872186876868cccf131adf978e
서민혁 <mahns1201@gmail.com>
2025-08-05 23:53:24 +0900

    using c_str(), atoi()

--- note/type.cpp
+++ note/type.cpp
@@ -1,5 +1,10 @@
 // char to string
 string(1, val);
 
-// string to int
-atoi(val);
+/* string to int
+    c_str(): c++ std::string 타입을 c의 char*, char[]로 변환해준다.
+    - "asdf" -> 0
+    - "0" -> 0
+    - "123" -> 123
+*/
+atoi(val.c_str());
