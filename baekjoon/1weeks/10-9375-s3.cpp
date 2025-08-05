5030d92c1b9341d3e5b3453826c04f284ff568c5
서민혁 <mahns1201@gmail.com>
2025-08-06 00:15:50 +0900

    [9375-s3] +3

--- baekjoon/1weeks/10-9375-s3.cpp
+++ baekjoon/1weeks/10-9375-s3.cpp
@@ -1,50 +1,85 @@
 // https://www.acmicpc.net/problem/9375
 
+/*
+	날짜: 2025-08-06
+	횟수: 3
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
-int P, C;
-string thing, type;
-
+int n, m;
+string s, ss;
 int main() {
-  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> n;
+    for (int i=0; i<n; i++) {
+        map<string, int> mp;
+        cin >> m;
+        
+        for (int i=0; i<m; i++) {
+            cin >> s >> ss;
+            mp[ss]++;
+        }
+        
+        int ret = 1;
+        for (auto it: mp) {
+            ret *= (it.second + 1);
+        }
 
-  cin >> P;
-  for (int i=0; i<P; i++) {
-    cin >> C;
-
-    map<string, int> li;
-    for (int j=0; j<C; j++) {
-      cin >> thing >> type;
-
-      li[type]++;
+        cout << ret - 1 << "\n";
     }
-    
-    long long ret = 1;
-    for (auto it : li) {
-      ret *= ((long long)it.second + 1);
-    }
-    
-    cout << ret - 1 << "\n";
-  }
 
-  return 0;
+    return 0;
 }
 
-int N;
-vector<int> ret;
+/*
+  횟수: 1
+*/
+// #include <bits/stdc++.h>
+// using namespace std;
 
-void printV(vector<int> V) {
-    for (auto i: V) {
-        cout << i << "\n";
-    }
-}
+// int P, C;
+// string thing, type;
+
+// int main() {
+//   ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+//   cin >> P;
+//   for (int i=0; i<P; i++) {
+//     cin >> C;
+
+//     map<string, int> li;
+//     for (int j=0; j<C; j++) {
+//       cin >> thing >> type;
+
+//       li[type]++;
+//     }
+    
+//     long long ret = 1;
+//     for (auto it : li) {
+//       ret *= ((long long)it.second + 1);
+//     }
+    
+//     cout << ret - 1 << "\n";
+//   }
+
+//   return 0;
+// }
+
+// int N;
+// vector<int> ret;
+
+// void printV(vector<int> V) {
+//     for (auto i: V) {
+//         cout << i << "\n";
+//     }
+// }
 
 
 /*
 	날짜: 2023-08-07
 	횟수: 2
-  특이사항: 필요 없는 코드 삭제할 것.
+    특이사항: 필요 없는 코드 삭제할 것.
 */
 // int main() {
 //     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
