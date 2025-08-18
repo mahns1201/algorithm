86863bc6cf6e57c0b2418592adb8fb324d0ebc7d
서민혁 <mahns1201@gmail.com>
2025-08-18 15:32:04 +0900

    Add input file

---
+++ note/input.cpp
@@ -0,0 +1,15 @@
+// 1. 입력이 없을 때까지 입력 받는법
+// 1.1. 정수
+int N;
+while (scanf("%d", &N) != EOF) {
+    a = 11;
+    ret = 2;
+    check();
+}
+
+// 1.2. 문자열
+string s;
+while (cin >> s) {
+    if (s == "end") break; 
+    
+}
