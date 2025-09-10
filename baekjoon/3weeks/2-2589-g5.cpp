daa2152f43264433a8e7dc30f9367b23d8cdccf1
서민혁 <mahns1201@gmail.com>
2025-09-10 16:00:00 +0900

    [2589-g5] +2*

--- baekjoon/3weeks/2-2589-g5.cpp
+++ baekjoon/3weeks/2-2589-g5.cpp
@@ -1,12 +1,80 @@
 // https://www.acmicpc.net/problem/2589
 
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+    날짜: 2025-09-10
+	횟수: 2
+    메모: 불필요한 조합 사용 X, 코드를 더 간결하게 작성할 수 있도록.
+*/
+
+string s;
+int dy[4] = { -1, 0, 1, 0 };
+int dx[4] = { 0, 1, 0, -1 };
+int n, m, y, x, result;
+int mp[54][54], visited[54][54];
+vector<pair<int, int>> v;
+
+void bfs(int ty, int tx) {
+    queue<pair<int, int>> q;
+    q.push({ ty, tx });
+    visited[ty][tx] = 1;
+
+    while(q.size()) {
+        tie(y, x) = q.front(); q.pop();
+
+        for (int t=0; t<4; t++) {
+            int ny = y + dy[t];
+            int nx = x + dx[t];
+
+            if (ny<0 || nx<0 || ny>=n || nx>=m) continue;
+            if (!mp[ny][nx] || visited[ny][nx]) continue;
+
+            q.push({ ny, nx });
+            visited[ny][nx] = visited[y][x] + 1;
+            result = max(result, visited[y][x]);
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> m;
+
+    for (int i=0; i<n; i++) {
+        cin >> s;
+        for (int j=0; j<m; j++) {
+            if (s[j] == 'L') {
+                mp[i][j] = 1;
+                v.push_back({ i, j });
+            }
+            else mp[i][j] = 0;
+        }
+    }
+
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            if (mp[i][j] == 1) {
+                memset(visited, 0, sizeof(visited));
+                bfs(i, j);
+            } 
+        }
+    }
+
+
+    cout << result << "\n";
+
+    return 0;
+}
+
 /*
     날짜: 2023-12-18
 	횟수: 1
     메모: memset으로 배열 초기화, bfs 공부 다시
 */
-#include <bits/stdc++.h>
-using namespace std;
 
 const int max_n = 54;
 int ret = -987654321;
