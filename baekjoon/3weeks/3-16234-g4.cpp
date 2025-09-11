6f20cceaa84c79367d0547da36f54e5e5104627f
서민혁 <mahns1201@gmail.com>
2025-09-11 09:59:44 +0900

    [16234-g4] +2

--- baekjoon/3weeks/3-16234-g4.cpp
+++ baekjoon/3weeks/3-16234-g4.cpp
@@ -1,13 +1,74 @@
 // https://www.acmicpc.net/problem/16234
 
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+    날짜: 2025-09-11
+	횟수: 2
+*/
+
+int dy[4] = { -1, 0, 1, 0 };
+int dx[4] = { 0, 1, 0, -1 };
+int n, l, r, _sum, _n, _cnt, result;
+int mp[54][54], visited[54][54];
+vector<pair<int, int>> v;
+
+void dfs(int y, int x) {
+    _sum += mp[y][x];
+    _n++;
+    v.push_back({ y, x });
+    visited[y][x] = 1;
+
+    for (int t=0; t<4; t++) {
+        int ny = y + dy[t];
+        int nx = x + dx[t];
+
+        if (ny<0 || nx<0 || ny>=n || nx>=n) continue;
+        if (abs(mp[ny][nx] - mp[y][x]) < l || abs(mp[ny][nx] - mp[y][x]) > r || visited[ny][nx]) continue;
+
+        dfs(ny, nx);
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> l >> r;
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<n; j++) cin >> mp[i][j];
+    }
+
+    while(_cnt < n*n) {
+        _cnt = 0;
+        memset(visited, 0, sizeof(visited));
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                _sum = 0;
+                _n = 0;
+                v.clear();
+                if (!visited[i][j]) {
+                    _cnt++;
+                    dfs(i, j);
+                    for (pair<int, int> it: v) mp[it.first][it.second] = _sum / _n;
+                }
+            }
+        }
+
+        if (_cnt != n*n) result++;
+    }
+
+    cout << result << "\n";
+
+    return 0;
+}
+
 /*
     날짜: 2023-12-19
 	횟수: 1
     메모: fill로 초기화, vector 초기화, *bfs에 추가 로직 구현
 */
-
-#include <bits/stdc++.h>
-using namespace std;
 
 const int max_n = 54;
 int ret = 0;
